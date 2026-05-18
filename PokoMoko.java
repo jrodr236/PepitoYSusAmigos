94938d8665b9e4573c941e2708d809e380c1e5d6
Eloi Gomez <egomezi962@boscdelacoma.cat>
2026-05-18 12:28:00 +0200

    PokoMoko

---
+++ PokoMoko.java
@@ -0,0 +1,25 @@
+public class PokoMoko extends Personatge{
+
+    public PokoMoko (String nom, Equip equip){
+        super (nom, equip);
+    }
+
+    @Override
+    public void atacar (Personatge oponent) {
+        int mal = Dau.tirar(10);
+        atacarAmbMal(oponent, mal);
+        augmentarMalFet(mal);
+    }
+
+    @Override
+    public void rebreMal(int mal){
+        int malAcabat = mal;
+
+        if (Dau.tirar(2) == 1){
+            malAcabat = Dau.tirar(mal +1);
+            System.out.println("S'ha defentat, mal reduit a:" + malAcabat);
+        }
+
+        super.rebreMal(malAcabat);
+    }
+}
