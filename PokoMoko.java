ad53466f1a27824219b78a6eee17f630275071de
Eloi Gomez <egomezi962@boscdelacoma.cat>
2026-05-18 13:38:42 +0200

    fer accio

--- PokoMoko.java
+++ PokoMoko.java
@@ -5,7 +5,7 @@ public class PokoMoko extends Personatge{
     }
 
     @Override
-    public void atacar (Personatge oponent) {
+    public void ferAccio (Personatge oponent) {
         int mal = Dau.tirar(10);
         atacarAmbMal(oponent, mal);
         augmentarMalFet(mal);
@@ -21,5 +21,6 @@ public class PokoMoko extends Personatge{
         }
 
         super.rebreMal(malAcabat);
+
     }
 }
